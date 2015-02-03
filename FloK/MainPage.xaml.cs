9a862d17cadc14d158088a88375473c9ff175415
unknown <orblanc@cotecolisson.com>
2015-02-03 22:48:06 +0100

    -Premiere page (test) sans accès bdd -Seconde page pour tester redirection

--- FloK/MainPage.xaml.cs
+++ FloK/MainPage.xaml.cs
@@ -16,9 +16,67 @@ namespace FloK
     public partial class MainPage : PhoneApplicationPage
     {
         // Constructeur
+
+        #region Constructor
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Fired when the user clicks on the 'Go' button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.tb_login.Text) && !string.IsNullOrEmpty(this.tb_pwd.Text))
+            {
+
+                // on va checker s'il existe dans la base, si oui; on le change d'écran (vers menu principale)
+
+                NavigationService.Navigate(new Uri("/MenuPage.xaml", UriKind.Relative));
+            }
+            else if (!string.IsNullOrEmpty(this.tb_login.Text))
+            {
+                this.tb_error.Text = "Please enter a password";
+            }
+            else if (!string.IsNullOrEmpty(this.tb_pwd.Text))
+            {
+                this.tb_error.Text = "Please enter a login";
+            }
+            // on ne fait rien car pas de données entrées
+            else { }
+        }
+
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tb_login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.tb_error.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tb_pwd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.tb_error.Text = string.Empty;
+        }
+
+        #endregion
+
     }
 }
